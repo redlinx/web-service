9610fca302b6ef80eb8b0df52f10618f25bca29d
ACER\Francis <francisreypadao@gmail.com>
2016-12-31 15:18:03 +0300

    This is a sample program in PHP about object oriented programming

---
+++ oop.php
@@ -0,0 +1,14 @@
+<!--Created by Francis Rey Padao-->
+<!--Date Created December 31 2016-->
+<?php
+class Student{
+	public function DisplayMessage(){
+		echo "Hello world";
+	}
+}
+
+$student = new Student();
+$student->DisplayMessage();
+
+
+?>
