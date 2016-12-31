0f5b275f77e4b1152740a771b703caec454ce8d4
ACER\Francis <francisreypadao@gmail.com>
2016-12-31 16:18:04 +0300

    no message

--- SOAP_Client.php
+++ SOAP_Client.php
@@ -1,6 +1,6 @@
 <?php
     ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
-    $client = new SoapClient("http://localhost/WebServiceSOAP2/inventory.wsdl");
-    $return = $client->getItemCount('123456');
+    $client = new SoapClient("http://10.53.48.12/webservice/server1.php");
+    $return = $client->displayStudentInfo();
     print_r($return);
 ?>
