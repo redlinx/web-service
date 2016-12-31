cf3c3ad4c40975c0fc4db7cea3cc291e8b4b1fa0
ACER\Francis <francisreypadao@gmail.com>
2016-12-31 16:53:34 +0300

    no message

--- server1.php
+++ server1.php
@@ -1,23 +1,12 @@
 <?php
+require_once('student.php');
 
-class student{
-	public function displayMessage($name){
-		$message = "Hello ".$name."<br/> How are you now?";
-		return $message;
-	}
-
-	public function add_values($x,$y){
-		$result = $x + $y;
-		return $result;
-	}
-}
+$server = new SoapServer(NULL, array('uri' => "http://10.53.48.12/webservice/server1.php"));
+$server->setClass('Student');
+$server->handle();
 
 
 
-
-
-$server = new SoapServer(NULL, array('uri' => "http://localhost/dayone/server1.php"));
-$server->setClass('student');
-$server->handle()$function_list = $server->getFunctions();
- print_r($function_list);
+// $function_list = $server->getFunctions();
+// print_r($function_list);
 ?>
