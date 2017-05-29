85f90b7f5d41eb59398b513a2bd474d2e21811f3
Francis Rey Padao <francisreypadao@gmail.com>
2017-05-29 21:40:06 +0300

    no message

--- server1/class/config.php
+++ server1/class/config.php
@@ -1,7 +1,28 @@
 <?php
-define("DATABASE", "aku");
-define("DB_HOSTNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-define("SERVER_HOSTNAME", "10.53.48.85");
+
+define("SERVER_HOSTNAME", "192.168.10.3");
+
+class DB_config{
+	protected function db_connection_details(){
+		$db_config[0]["db_name"] = "aku";
+		$db_config[0]["host_address"] = "192.168.0.101";
+		$db_config[0]["username"] = "user1";
+		$db_config[0]["password"] = "user1";
+
+		/*
+		$db_config[1]["db_name"] = "aku";
+		$db_config[1]["host_address"] = "192.168.10.6";
+		$db_config[1]["username"] = "pc5";
+		$db_config[1]["password"] = "pc5";
+
+		$db_config[2]["db_name"] = "aku";
+		$db_config[2]["host_address"] = "192.168.10.11";
+		$db_config[2]["username"] = "pc11";
+		$db_config[2]["password"] = "pc11";
+		*/
+
+		return $db_config;
+	}
+}
+
 ?>
